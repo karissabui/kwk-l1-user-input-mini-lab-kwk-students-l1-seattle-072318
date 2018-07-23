75176c7d550be3d17d5ee9a3d09119feaed80310
Karissa Bui <karissab002@gmail.com>
2018-07-23 22:41:12 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -1,5 +1,10 @@
-# Code your prompts here!
-
+puts "Let's plan your trip!Where would you like to stay?"
+stay + = gets.chomp.capitalize
+puts "Great!You will stay in a #{stay}! What would you like to eat during your stay?"
+eat + = gets.chomp.capitalize
+puts "I love #{eat}!How many nights would you like to stay in your #{stay}"
+time + = gets.chomp.capitalize
+puts "Fantastic! hope you enjoy your stay at your #{stay}"
 # First, puts out a string asking where the tourist would like to stay.
 puts "Let's plan your trip! Where would you like to stay?"
 
